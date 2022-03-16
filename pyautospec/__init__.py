4409a137d74ec78749019ef43c4ef5930f6d1396
Luca Marx <luca@lucamarx.com>
2022-03-16 14:35:08 +0100

    Fixed imports

--- pyautospec/__init__.py
+++ pyautospec/__init__.py
@@ -6,8 +6,7 @@ Spectral learning for WFA
 
 from .wfa import Wfa
 from .spectral_learning import SpectralLearning
-from .image_wfa import ImageWfa
 from .function_wfa import FunctionWfa
 
 
-__all__ = ["Wfa", "SpectralLearning", "ImageWfa", "FunctionWfa"]
+__all__ = ["Wfa", "SpectralLearning", "FunctionWfa"]
