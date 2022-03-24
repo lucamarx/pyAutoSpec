b98a796d852a2fd8f6e08edebdf8c2bc5ee3f3c9
Luca Marx <luca@lucamarx.com>
2022-03-24 11:55:54 +0100

    Implemented WFA based image compression

--- pyautospec/__init__.py
+++ pyautospec/__init__.py
@@ -7,6 +7,6 @@ Spectral learning for WFA
 from .wfa import Wfa
 from .spectral_learning import SpectralLearning
 from .function_wfa import FunctionWfa
+from .image_wfa import ImageWfa
 
-
-__all__ = ["Wfa", "SpectralLearning", "FunctionWfa"]
+__all__ = ["Wfa", "SpectralLearning", "FunctionWfa", "ImageWfa"]
---
+++ pyautospec/image_wfa.py
@@ -0,0 +1,90 @@
+"""
+Wfa based image compression algorithm
+"""
+import itertools
+import numpy as np
+
+from tqdm.auto import tqdm
+from skimage import color
+from .spectral_learning import SpectralLearning
+
+
+def decode(w : str, tl=(0, 0), sz=100):
+    """
+    Decode word → x-y coordinates, square size
+    """
+    if w == "":
+        return tl, sz
+
+    sz = sz // 2
+
+    if w[0] == "b":
+        tl = (tl[0]+sz, tl[1])
+    elif w[0] == "c":
+        tl = (tl[0], tl[1]+sz)
+    elif w[0] == "d":
+        tl = (tl[0]+sz, tl[1]+sz)
+
+    return decode(w[1:], tl, sz)
+
+
+def get_image(image, w : str):
+    (x, y), s = decode(w, sz=image.shape[0])
+
+    # average image over square
+    return image[x, y] if s == 0 else np.average(image[x:(x+s), y:(y+s)])
+
+
+class ImageWfa():
+
+    def __init__(self, image):
+        """
+        Intialize image model
+        """
+        # convert to grayscale
+        if len(image.shape) == 3:
+            if image.shape[2] == 3:
+                image = color.rgb2gray(image)
+            elif image.shape[2] == 4:
+                image = color.rgb2gray(color.rgba2rgb(image))
+            else:
+                raise Exception("Invalid image shape")
+
+        # crop
+        w, h = image.shape
+        if w > h:
+            image = image[(w-h) // 2 : h + (w-h) // 2, :]
+        elif w < h:
+            image = image[:, (h-w) // 2 : w + (h-w) // 2]
+
+        self.image = image / np.max(image)
+
+
+    def learn(self, resolution=4):
+        """
+        Learn a model of the image
+        """
+        sl = SpectralLearning(["a", "b", "c", "d"], resolution)
+        self.model = sl.learn(lambda w: get_image(self.image, w))
+
+
+    def reconstruct(self, resolution=5):
+        """
+        Reconstruct the image
+        """
+        image = np.zeros(self.image.shape)
+        error, count = 0, 0
+        for w in tqdm([''.join(w) for w in itertools.product(*([["a", "b", "c", "d"]] * resolution))]):
+            (x, y), s = decode(w, sz=image.shape[0])
+
+            v0 = self.image[x, y] if s == 0 else np.average(self.image[x:(x+s), y:(y+s)])
+            v1 = self.model.evaluate(w)
+
+            error += abs(v1 - v0)
+            count += 1
+
+            image[x:(x+s), y:(y+s)] = v1
+
+        image[image < 0] = 0
+
+        return image, error/count
