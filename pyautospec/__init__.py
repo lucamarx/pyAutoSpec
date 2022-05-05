48a6e476172611613855b23b2ca3cb8319294cf6
Luca Marx <luca@lucamarx.com>
2022-05-05 16:18:12 +0200

    Updated module exports

--- pyautospec/__init__.py
+++ pyautospec/__init__.py
@@ -4,10 +4,11 @@ pyAutoSpec
 Spectral learning for WFA/MPS
 """
 
-from .mps import Mps
 from .wfa import Wfa
+from .mps import Mps, SymbolicMps
 from .spectral_learning import SpectralLearning
 from .function_wfa import FunctionWfa
+from .function_mps import FunctionMps
 from .image_wfa import ImageWfa
 
-__all__ = ["Mps", "Wfa", "SpectralLearning", "FunctionWfa", "ImageWfa"]
+__all__ = ["Wfa", "Mps", "SymbolicMps", "SpectralLearning", "FunctionWfa", "FunctionMps", "ImageWfa"]
