6b30f285ba275658281cd938da347d175e2d4470
Luca Marx <luca@lucamarx.com>
2022-05-07 16:45:56 +0200

    Fixed model exports

--- pyautospec/__init__.py
+++ pyautospec/__init__.py
@@ -5,9 +5,10 @@ Spectral learning for WFA/MPS
 """
 
 from .wfa import Wfa, SpectralLearning
-from .mps import Mps, SymbolicMps
+from .mps import MpsR, MpsC, SymbolicMps
 from .function_wfa import FunctionWfa
 from .function_mps import FunctionMps
+from .dataset_mps import DatasetMps
 from .image_wfa import ImageWfa
 
-__all__ = ["Wfa", "Mps", "SymbolicMps", "SpectralLearning", "FunctionWfa", "FunctionMps", "ImageWfa"]
+__all__ = ["Wfa", "MpsR", "MpsC", "SymbolicMps", "SpectralLearning", "FunctionWfa", "FunctionMps", "DatasetMps", "ImageWfa"]
