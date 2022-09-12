Fixed singular value normalization

pyautospec/mps.py
@@ -391,8 +391,8 @@ class Mps:
         remaining (N-n) particles
         """
         if n is None:
-            sv2s = [np.square(sv / np.sum(sv)) for sv in self.singular_values]
+            sv2s = [sv2 / np.sum(sv2) for sv2 in [np.square(sv) for sv in self.singular_values]]
             return [-np.einsum("i,i->", sv2, np.log2(sv2)).item() for sv2 in sv2s]
         else:
-            sv2 = np.square(self.singular_values[n] / np.sum(self.singular_values[n]))
+            sv2 = np.square(self.singular_values[n]) / np.sum(np.square(self.singular_values[n]))
             return -np.einsum("i,i->", sv2, np.log2(sv2)).item()