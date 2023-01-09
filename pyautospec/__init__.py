65792ee96fb7b62345fbaab01a6083474c5192ea
Luca Marx <luca@lucamarx.com>
2023-01-09 13:58:20 +0100

    Started implementing uMPS based functions

--- pyautospec/__init__.py
+++ pyautospec/__init__.py
@@ -1,7 +1,7 @@
 """
 pyAutoSpec
 
-Spectral learning for WFA/MPS
+Spectral learning for WFA/MPS/uMPS
 """
 
 from .wfa import Wfa, SpectralLearning
@@ -12,10 +12,12 @@ from .plots import parallel_plot
 from .datasets import load_mnist
 from .function_wfa import FunctionWfa
 from .function_mps import FunctionMps
+from .function_umps import FunctionUMps
 from .dataset_mps import DatasetMps
 from .image_wfa import ImageWfa
 
 __all__ = ["Wfa", "Mps", "Mps2", "UMPS", "parallel_plot", "load_mnist",
-           "SpectralLearning", "FunctionWfa", "FunctionMps", "DatasetMps", "ImageWfa"]
+           "SpectralLearning", "FunctionWfa", "FunctionMps", "FunctionUMps",
+           "DatasetMps", "ImageWfa"]
 
 __version__ = "0.9.1"
---
+++ pyautospec/function_umps.py
@@ -0,0 +1,47 @@
+"""
+UMps based multi-dimensional functions
+"""
+from typing import List, Tuple, Optional, Callable
+
+from .umps import UMPS
+from .encoder import VectorEncoder
+
+
+class FunctionUMps():
+    """
+    UMps based multi-dimensional function
+    """
+
+    def __init__(self, limits : List[Tuple[float, float]], encoding_length : Optional[int] = 8, max_bond_dim : Optional[int] = 20):
+        """Create a multi dimensional real function
+
+        Paramaters
+        ----------
+
+        limits : List[Tuple[float,float]]
+        The limits of each vector dimension
+
+        """
+        self.encoder = VectorEncoder(limits, encoding_length)
+        self.umps = UMPS(self.encoder.part_d, max_bond_dim)
+        self.f = None
+
+
+    def __repr__(self):
+        return f"""
+  {" x ".join([f"[{x0:.2f},{x1:.2f})" for (x0, x1) in self.encoder.limits])} → R
+  {self.umps.__repr__()}
+        """
+
+
+    def __call__(self, *args) -> float:
+        """Evaluate"""
+        return self.umps(self.encoder.encode(*args))
+
+
+    def learn(self, f : Callable[[Tuple], float], learn_resolution : int):
+        """Learn a function
+
+        """
+        self.f = f
+        self.umps.learn_function(lambda x: f(*self.encoder.decode(x)), learn_resolution)
